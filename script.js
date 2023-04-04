16ac7cfd56acbba8a669c6e25936810f2be74daa
Arnel Isiderio Robles <arnelirobles@outlook.com>
2023-04-04 16:21:53 +0800

    add local session storage, clear history, paging

--- script.js
+++ script.js
@@ -44,10 +44,12 @@ function renderHistory() {
         historyBody.appendChild(row);
     }
 
-    pageInfo.textContent = `Page ${currentPage}`;
+    const totalPages = Math.ceil(historyData.length / pageSize);
+    pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
     prevBtn.disabled = currentPage === 1;
     nextBtn.disabled = end === historyData.length;
 }
+
 
 function flipCoin(playerChoice) {
     const randomOutcome = Math.random() < 0.5 ? "Heads" : "Tails";
@@ -77,7 +79,66 @@ function prevPage() {
     renderHistory();
 }
 
+// ... (previous script.js content)
+
+function loadHistoryFromStorage() {
+    const historyJSON = localStorage.getItem("flipCoinHistory");
+    const timestamp = localStorage.getItem("flipCoinHistoryTimestamp");
+
+    if (historyJSON && timestamp) {
+        const now = new Date().getTime();
+        const age = now - parseInt(timestamp, 10);
+
+        // Clear history if it's older than 24 hours (24 * 60 * 60 * 1000 milliseconds)
+        if (age > 24 * 60 * 60 * 1000) {
+            localStorage.removeItem("flipCoinHistory");
+            localStorage.removeItem("flipCoinHistoryTimestamp");
+            historyData = [];
+        } else {
+            historyData = JSON.parse(historyJSON);
+            games = historyData.length;
+            wins = historyData.filter(item => item.winLoss === "Win").length;
+            gamesPlayed.textContent = games;
+            gamesWon.textContent = wins;
+            renderHistory();
+        }
+    }
+}
+
+function saveHistoryToStorage() {
+    const historyJSON = JSON.stringify(historyData);
+    const timestamp = new Date().getTime();
+    localStorage.setItem("flipCoinHistory", historyJSON);
+    localStorage.setItem("flipCoinHistoryTimestamp", timestamp);
+}
+
+function addToHistory(gameNumber, outcome, winLoss) {
+    historyData.push({ gameNumber, outcome, winLoss });
+    saveHistoryToStorage();
+    renderHistory();
+}
+
+loadHistoryFromStorage();
+
 headsBtn.addEventListener("click", () => flipCoin("Heads"));
 tailsBtn.addEventListener("click", () => flipCoin("Tails"));
 nextBtn.addEventListener("click", nextPage);
 prevBtn.addEventListener("click", prevPage);
+
+
+const clearHistoryBtn = document.getElementById("clearHistoryBtn");
+
+function clearHistory() {
+    localStorage.removeItem("flipCoinHistory");
+    localStorage.removeItem("flipCoinHistoryTimestamp");
+    historyData = [];
+    games = 0;
+    wins = 0;
+    currentPage = 1;
+    gamesPlayed.textContent = games;
+    gamesWon.textContent = wins;
+    renderHistory();
+}
+
+clearHistoryBtn.addEventListener("click", clearHistory);
+
